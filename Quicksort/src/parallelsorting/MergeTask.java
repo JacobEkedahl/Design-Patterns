0afce1a3aff7587cb1856b0623fdd54ff39b3205
nordpool <beelzenord@gmail.com>
2019-02-21 13:28:21 +0100

    Merge origin/master
    Conflicts:
    	Quicksort/src/quicksort/QuicksortTask.java
    	Quicksort/src/quicksort/Task1.java

---
+++ Quicksort/src/parallelsorting/MergeTask.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parallelsorting;
+import java.util.concurrent.RecursiveAction;
+import java.util.concurrent.RecursiveTask;
+import mergeSort.ParallelMerge;
+
+public class MergeTask extends RecursiveAction {
+    
+    private float[] array;
+    private final int start, stop;
+    private long res;
+
+    public MergeTask(float[] array, int start, int stop) {
+        this.array = array;
+        this.start = start;
+        this.stop = stop;
+    }
+
+
+    @Override
+    protected void compute() {
+        if(start - stop < Task1.THRESHOLD){
+            ParallelMerge.mergeSort(array);
+          
+        }
+        else{
+            int mid = (start+stop)/2;
+            MergeTask worker1 = new MergeTask(array,start,mid);
+            MergeTask worker2 = new MergeTask(array,mid+1,stop);
+            worker1.fork();
+            worker2.compute();
+            worker1.join();
+        }
+
+
+
+
+    }
+}
--- Quicksort/src/parallelsorting/Task1.java
+++ Quicksort/src/parallelsorting/Task1.java
@@ -5,6 +5,12 @@
  */
 package parallelsorting;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ForkJoinPool;
@@ -19,68 +25,77 @@ public class Task1 {
     /**
      * @param args the command line arguments
      */
-    static int MAX_THRESHOLD = 100000;
-    static int MIN_THRESHOLD = 100;
-    static int INTERVAL = 1000;
+    static int MAX_THRESHOLD = 20;
+    static int MIN_THRESHOLD = 10;
+    static int INTERVAL = 5;
     public static int THRESHOLD = 10000;
+
+    
     int currentCoreIndex = 1;
+
+    //set the type of sorting to use inside start_task method
     static final int QUICKSORT = 0;
     static final int ARRAY_SORT = 1;
-    static final int MERGE_SORT = 2;
+    static final int MERGESORT = 2;
 
-    public static int ITERATIONS = 255;
+    public static int ITERATIONS = 15;
     public static int START_INDEX = 5;
-    public static int SIZE_ARRAY = (int) 10000000;
+    public static int SIZE_ARRAY = (int) 100000;
 
     static long totalTime = 0;
     static int cores;
 
-    public static void main(String[] args) throws InterruptedException {
-        cores = Runtime.getRuntime().availableProcessors();
-        start_task(ARRAY_SORT);
+    static String output_file = "mergesort";
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        cores =Runtime.getRuntime().availableProcessors();
+        output_file += "_" + cores;
+        prepareFile(output_file);
+        System.out.println("cores: " + cores);
+        start_task(MERGESORT);
     }
 
-    private static void start_task(int type) {
-        // for (int j = MIN_THRESHOLD; j < MAX_THRESHOLD; j += INTERVAL) {
-        //   THRESHOLD = j;
+    private static void start_task(int type) throws InterruptedException {
+        for (int j = MIN_THRESHOLD; j <= MAX_THRESHOLD; j += INTERVAL) {
+            THRESHOLD = j;
 
-        for (int i = 0; i < ITERATIONS; i++) {
-            float[] arr = gen_random_arr(SIZE_ARRAY);
-            switch (type) {
-              
-                case ARRAY_SORT:
-                    array_sort(arr, i);
-                    break;
-                case QUICKSORT:
-                    parallel_quicksort(arr, i, cores);
-                    break;
-                case MERGE_SORT:
-                    
-                        
+            for (int i = 0; i < ITERATIONS; i++) {
+                float[] arr = gen_random_arr(SIZE_ARRAY);
+                switch (type) {
+                    case ARRAY_SORT:
+                        array_sort(arr, i);
+                        break;
+                    case QUICKSORT:
+                        parallel_quicksort(arr, i, cores);
+                        break;
+                    case MERGESORT:
+                        parallel_mergesort(arr,i,cores);
+                }
+                System.gc();
             }
-            System.gc();
-        }
 
-        int totalRecordings = ITERATIONS - START_INDEX;
-        float average = (float) totalTime / totalRecordings / 1000000;
-        System.out.println("average of " + totalRecordings + " runs: " + average + " ms" + ", threshold: " + THRESHOLD);
-        System.gc();
-        //  }
+            int totalRecordings = ITERATIONS - START_INDEX;
+            float average = (float) totalTime / totalRecordings / 1000000;
+            System.out.println("average of " + totalRecordings + " runs: " + average + " ms" + ", threshold: " + THRESHOLD);
+            saveResult(THRESHOLD, average, cores, totalRecordings, output_file);
+            System.gc();
+            totalTime = 0;
+        }
     }
-    
+
     private static void array_sort(float arr[], int currIndex) {
         long startTime = System.nanoTime();
         Arrays.parallelSort(arr);
         long endTime = System.nanoTime();
 
         long elapsedTime = endTime - startTime;
-        if (currIndex - 1 >= START_INDEX) {
+        if (currIndex >= START_INDEX) {
             totalTime += elapsedTime;
         }
-        
+
         System.out.println("time: " + elapsedTime + ", is sorted: " + isSorted(arr));
     }
-    
+
     private static void parallel_quicksort(float[] arr, int currIndex, int cores) {
         ForkJoinPool pool = new ForkJoinPool(cores);
         QuicksortTask rootTask = new QuicksortTask(arr, 0, arr.length - 1);
@@ -90,16 +105,15 @@ public class Task1 {
         long endTime = System.nanoTime();
 
         long elapsedTime = endTime - startTime;
-        if (currIndex - 1 >= START_INDEX) {
+        if (currIndex >= START_INDEX) {
             totalTime += elapsedTime;
         }
 
         pool.shutdown();
         System.out.println("time: " + elapsedTime + ", is sorted: " + isSorted(arr));
     }
-
-
-    private static void merge_sort(float[] arr, int currIndex, int cores) {
+    
+    private static void parallel_mergesort(float[] arr, int currIndex, int cores) {
         ForkJoinPool pool = new ForkJoinPool(cores);
         MergeTask rootTask = new MergeTask(arr, 0, arr.length - 1);
 
@@ -108,12 +122,12 @@ public class Task1 {
         long endTime = System.nanoTime();
 
         long elapsedTime = endTime - startTime;
-        if (currIndex - 1 >= START_INDEX) {
+        if (currIndex >= START_INDEX) {
             totalTime += elapsedTime;
         }
 
         pool.shutdown();
-        System.out.println("time: " + elapsedTime + ", is sorted: " + isSorted(arr));
+        System.out.println("time: " + elapsedTime + ", is sorted: " + isSorted(arr)); //To change body of generated methods, choose Tools | Templates.
     }
 
     //test method for validating the sorting of array
@@ -136,5 +150,26 @@ public class Task1 {
         }
 
         return arr;
+    }
+
+    public static void saveResult(int threshold, float time, int cores, int totalRecordings, String output) {
+        PrintWriter pw;
+        try {
+            pw = new PrintWriter(new BufferedWriter(new FileWriter(output + ".txt", true)));
+
+            pw.println();
+            pw.print(SIZE_ARRAY + " " + threshold + " " + time + " " + cores + " " + totalRecordings);
+            pw.flush();
+            pw.close();
+
+        } catch (Exception ex) {
+            // do nothing
+        }
+    }
+
+    public static void prepareFile(String output) throws IOException {
+        Writer fileWriter = new FileWriter(output + ".txt");
+        fileWriter.write("size threshold time cores iterations");
+        fileWriter.close();
     }
 }
