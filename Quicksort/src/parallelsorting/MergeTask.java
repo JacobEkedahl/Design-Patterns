8f8bd345d869a88c8e9171b578ebec10c9755ac1
JacobEkedahl <jacob.ekedahl@gmail.com>
2019-02-21 22:51:55 +0100

    Adding parallel mergesort

--- Quicksort/src/parallelsorting/MergeTask.java
+++ Quicksort/src/parallelsorting/MergeTask.java
@@ -4,15 +4,20 @@
  * and open the template in the editor.
  */
 package parallelsorting;
-import java.util.concurrent.RecursiveAction;
-import java.util.concurrent.RecursiveTask;
-import mergeSort.ParallelMerge;
 
+import java.util.Arrays;
+import java.util.concurrent.RecursiveAction;
+
+/**
+ *
+ * @author Jacob
+ */
 public class MergeTask extends RecursiveAction {
-    
-    private float[] array;
+
+    private static final int THRESHOLD = 128;
+
+    private final float[] array;
     private final int start, stop;
-    private long res;
 
     public MergeTask(float[] array, int start, int stop) {
         this.array = array;
@@ -20,25 +25,60 @@ public class MergeTask extends RecursiveAction {
         this.stop = stop;
     }
 
+    public void sort() {
+        this.compute();
+    }
 
     @Override
     protected void compute() {
-        if(start - stop < Task1.THRESHOLD){
-            ParallelMerge.mergeSort(array);
-            
+        if (start < stop) {
+            int size = stop - start;
+            if (size < Task1.THRESHOLD) {
+                //Arrays.parallelSort(array, start, stop);
+                //insertionsort();
+                Arrays.sort(array, start, stop);
+            } else {
+                int m = (start + stop) / 2;
+                invokeAll(
+                        new MergeTask(array, start, m),
+                        new MergeTask(array, m + 1, stop));
+
+                merge(m);
+            }
         }
-        else{
-            System.out.println("making workers");
-            int mid = (start+stop)/2;
-            MergeTask worker1 = new MergeTask(array,start,mid);
-            MergeTask worker2 = new MergeTask(array,mid+1,stop);
-            worker1.fork();
-            worker2.compute();
-            worker1.join();
+    }
+
+    private void insertionsort() {
+        for (int i = start + 1; i <= stop; i++) {
+            float key = array[i];
+            int j = i - 1;
+
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+
+            array[j + 1] = key;
         }
+    }
 
+    private void merge(int m) {
+        float[] a = Arrays.copyOfRange(array, start, m + 1);
+        float[] b = Arrays.copyOfRange(array, m + 1, stop + 1);
+        int indexa = 0, indexb = 0, indexc = start;
 
-
-
+        while (indexa < a.length && indexb < b.length) {
+            if (a[indexa] <= b[indexb]) {
+                array[indexc++] = a[indexa++];
+            } else {
+                array[indexc++] = b[indexb++];
+            }
+        }
+        while (indexa < a.length) {
+            array[indexc++] = a[indexa++];
+        }
+        while (indexb < b.length) {
+            array[indexc++] = b[indexb++];
+        }
     }
 }
