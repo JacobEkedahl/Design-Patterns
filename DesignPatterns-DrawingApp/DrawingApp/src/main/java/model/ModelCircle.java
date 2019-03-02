9732af6df91f0f9a8e25f8fa8fe3332ede68c397
nordpool <beelzenord@gmail.com>
2019-03-02 19:06:59 +0100

    Merge origin/master
    Conflicts:
    	Quicksort/cores_sorting.txt
    	Quicksort/src/parallelsorting/Task1.java

---
+++ DesignPatterns-DrawingApp/DrawingApp/src/main/java/model/ModelCircle.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+import javafx.scene.shape.*;
+
+/**
+ *
+ * @author Jacob
+ */
+public class ModelCircle extends Shape {
+    public ModelCircle(float fromX, float fromY, float toX, float toY) {
+        super(fromX, fromY, toX, toY);
+    }
+
+    @Override
+    void drawShape() {
+      Circle circle = new Circle();
+      
+      float fromX = getFromX();
+      float fromY = getFromY();
+      
+      float a = getFromX() - getToX();
+      float b = getFromY() - getToY();
+      
+      float pyth = (float)Math.sqrt((a*a) + (b*b));
+      
+      circle.setCenterX(fromX);
+      circle.setCenterY(fromY);
+      circle.setRadius(pyth);
+      
+    }
+}
---
+++ DesignPatterns-DrawingApp/DrawingApp/src/main/java/model/ModelLine.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+import javafx.scene.shape.*;
+
+/**
+ *
+ * @author Jacob
+ */
+public class ModelLine extends Shape {
+    public ModelLine(float fromX, float fromY, float toX, float toY) {
+        super(fromX, fromY, toX, toY);
+    }
+
+    @Override
+    void drawShape() {
+        Line l = new javafx.scene.shape.Line();
+        
+        l.setStartX(getFromX());
+        l.setStartY(getFromY());
+        l.setEndX(getToX());
+        l.setEndY(getToY());
+        
+       
+    }
+}
