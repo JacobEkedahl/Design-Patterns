7c5f77b94f146107c6479de91b8131de7a5578e7
JacobEkedahl <jacob.ekedahl@gmail.com>
2019-03-03 19:09:04 +0100

    Refactoring..

--- DesignPatterns-DrawingApp/DrawingApp/src/main/java/controller/FXMLController.java
+++ DesignPatterns-DrawingApp/DrawingApp/src/main/java/controller/FXMLController.java
@@ -1,4 +1,4 @@
-package com.kanonkod.drawingapp;
+package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
--- DesignPatterns-DrawingApp/DrawingApp/src/main/java/view/MainApp.java
+++ DesignPatterns-DrawingApp/DrawingApp/src/main/java/view/MainApp.java
@@ -1,4 +1,4 @@
-package com.kanonkod.drawingapp;
+package view;
 
 import javafx.application.Application;
 import static javafx.application.Application.launch;
